90a321004db78a61d9bff6b8b744abcccf0523be
harelpanker <harelpanker@gmail.com>
2023-10-15 20:40:43 +0200

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="flex min-h-100dvh flex-col justify-between">
+	<div>
+		<!-- <Navbar /> -->
+		<main class="relative z-10 overflow-hidden py-10 xl:py-16">
+			<slot />
+		</main>
+	</div>
+	<!-- <Footer /> -->
+</div>
